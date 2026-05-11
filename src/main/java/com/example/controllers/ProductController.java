6a90b2751fa19d135ed0c42ed4c887b94a8a844c
Ogisawa <kazuma.og525@gmail.com>
2026-05-11 12:08:18 +0900

    HttpSessionを使ったカート機能を追加

--- src/main/java/com/example/controllers/ProductController.java
+++ src/main/java/com/example/controllers/ProductController.java
@@ -1,6 +1,5 @@
 package com.example.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,57 +7,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.example.ecsample.entity.Product;
+import com.example.entity.Product;
+import com.example.mapper.ProductMapper;
 
 @Controller
 public class ProductController {
+	private final ProductMapper productMapper;
+
+	public ProductController(ProductMapper productMapper) {
+		this.productMapper = productMapper;
+	}
 
 	@GetMapping("/products")
 	public String showList(Model model) {
-		//		// 仮のデータをべた書き
-		//		String[][] products = {
-		//				{ "1", "コーヒーカップ", "1200" },
-		//				{ "2", "ティーポット", "2400" },
-		//				{ "3", "マグカップ", "1500" }
-		//		};
 
-		List<Product> products = Arrays.asList(
-				new Product(1, "コーヒーカップ", 1200),
-				new Product(2, "ティーポット", 2400),
-				new Product(3, "マグカップ", 1500));
+		List<Product> products = productMapper.findAll();
 		model.addAttribute("products", products);
 		return "product/list";
 	}
 
 	@GetMapping("/product/{id}")
 	public String showDetail(@PathVariable("id") int id, Model model) {
-		//		String name;
-		//		int price;
-		//
-		//		// ID によって商品を切り替える(仮のデータ）
-		//		if (id == 1) {
-		//			name = "コーヒーカップ";
-		//			price = 1200;
-		//		} else if (id == 2) {
-		//			name = "ティーポット";
-		//			price = 2400;
-		//		} else {
-		//			name = "未登録の商品";
-		//			price = 0;
-		//		}
-		//
-		//		model.addAttribute("id", id);
-		//		model.addAttribute("name", name);
-		//		model.addAttribute("price", price);
 
-		Product product;
-		if (id == 1) {
-			product = new Product(1, "コーヒーカップ", 1200);
-		} else if (id == 2) {
-			product = new Product(2, "ティーポット", 2400);
-		} else {
-			product = new Product(0, "未登録の商品", 0);
-		}
+		Product product = productMapper.findById(id);
 		model.addAttribute("product", product);
 
 		//商品詳細ページを表示する
--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -1,27 +1,32 @@
-package com.example.ecsample.entity;
+package com.example.entity;
 
 public class Product {
 	private int id;
 	private String name;
 	private int price;
 
-	public Product(int id, String name, int price) {
-		this.id = id;
-		this.name = name;
-		this.price = price;
-
-	}
-
 	public int getId() {
 		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public int getPrice() {
 		return price;
-
 	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
 }
